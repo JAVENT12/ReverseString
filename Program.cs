7f3ede724eb514604d59c700c0633180014d9c10
Jason Ventura <ventj2014@gmail.com>
2019-08-02 21:23:24 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ReverseString
+{
+    public class Program
+    {
+        public static string mirror(string name1)
+        {
+            string reverse = name1;
+            string word = ""; 
+            for (int i = 1; i <= reverse.Length; i++)
+            {
+                word += reverse[reverse.Length - i];
+            }
+            return word;
+        }
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            Console.WriteLine(mirror("Jason"));
+        }
+    }
+}
